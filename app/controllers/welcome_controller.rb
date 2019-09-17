61b38497b1441b1dfc207d884ce1725fdfdc17f6
bbover <o0supreme0o@gmail.com>
2019-09-17 21:04:07 +0800

    update sth.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+    def index
+        flash[:notice] = 'morning!'
+        flash[:alert] = 'good night!'
+        flash[:warning] = 'warning!'
+    end
 end
