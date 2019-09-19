11abb16588f7730d1c66f38cede6957b0036ef9e
bbover <o0supreme0o@gmail.com>
2019-09-19 16:49:54 +0800

    implement edit/delete of my posts page

--- app/controllers/account/groups_controller.rb
+++ app/controllers/account/groups_controller.rb
@@ -1,5 +1,6 @@
 class Account::GroupsController < ApplicationController
     before_action :authenticate_user!
+    before_action :find_group_and_check_permission, only:[:edit,:update,:destroy]
     def index
         @groups = current_user.participated_groups
     end
